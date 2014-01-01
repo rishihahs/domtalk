fd1a7bd23f10a9b5ce2a460c3449cb9ef7a45d10
Rishi Shah <rishihahs@gmail.com>
2014-01-01 15:47:12 -0600

    Add tests

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var test = require('tape');
+var html = require('./html');
+
+var domtalk = require('../');
+
+html(); // Set up mock html page;
+
+test('css selector from DOM element', function(t) {
+    t.plan(1);
+
+    var el = document.getElementsByTagName('a')[2];
+    var selector = 'body *:nth-child(1) *:nth-child(1) *:nth-child(1) *:nth-child(3) *:nth-child(1)';
+
+    t.equal(domtalk.getSelectorFromElement(el), selector);
+});
+
+test('DOM element from css selector', function(t) {
+    t.plan(1);
+
+    var el = document.getElementsByTagName('a')[2];
+    var selector = 'body *:nth-child(1) *:nth-child(1) *:nth-child(1) *:nth-child(3) *:nth-child(1)';
+
+    t.equal(domtalk.getElementFromSelector(selector), el);
+});
